8ed76c984c6594a23da05b150eb078660ae45ec0
Olga <olya.debelaya@gmail.com>
2022-05-30 11:48:16 +0300

    main page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import 'regenerator-runtime/runtime';
-
-import { API_URL, URL_COMICS } from './constants/api';
-import {getDataApi} from './utils/getDataApi';
+import App from './components/App';
+import Comics from './components/Comics';
 
 (async () => {
-    const data = await getDataApi.getData(API_URL + URL_COMICS);
-    console.log(data);
+    await App.render();
+Comics.eventListener();
 })();
