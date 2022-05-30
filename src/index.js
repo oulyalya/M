17b4919783daba6ad567e691b9245d437905e65d
Olga <olya.debelaya@gmail.com>
2022-05-30 09:52:47 +0300

    structure and data

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime/runtime';
+
+import { API_URL, URL_COMICS } from './constants/api';
+import {getDataApi} from './utils/getDataApi';
+
+(async () => {
+    const data = await getDataApi.getData(API_URL + URL_COMICS);
+    console.log(data);
+})();
