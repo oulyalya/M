e50626efd9a90247711e5f3bbfa87c4379f13379
Olga <olya.debelaya@gmail.com>
2022-05-31 07:07:27 +0300

    main page

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -2,12 +2,12 @@ import { API_URL, URL_COMICS, URL_CHARACTERS, IMG_STANDARD_XLARGE, IMG_NOT_AVAIL
 import { getDataApi } from '../../utils/getDataApi';
 import { ROOT_INDEX } from '../../constants/root';
 
+import Error from '../Error';
+
 import './Comics.css';
 
 class Comics {
-    async render() {
-        const data = await getDataApi.getData(API_URL + URL_COMICS);
-
+    renderComics(data) {
         let htmlContent = '';
 
         data.forEach(({ id, title, thumbnail: { path, extension } }) => {
@@ -31,6 +31,16 @@ class Comics {
         `;
 
         ROOT_INDEX.innerHTML = htmlWrapper;
+    }
+
+    async render() {
+        const data = await getDataApi.getData(API_URL + URL_COMICS);
+
+        if (data) {
+            this.renderComics(data);
+        } else {
+            // Error.render();
+        }
     }
 
     eventListener() {
